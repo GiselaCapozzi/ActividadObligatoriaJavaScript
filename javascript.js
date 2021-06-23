35d782020da856c8798d5f4267aba0437c47c328
GiselaCapozzi <gisecapozzi@gmail.com>
2021-06-23 15:49:15 -0300

    first commit

---
+++ javascript.js
@@ -0,0 +1,5 @@
+var primera = 40;
+var segunda = 16;
+var sumaTotal = primera + segunda;
+
+document.write("<h1 style='color:blue'>El resultado de la suma de las dos variables es: </h1> " + "<h3>" + sumaTotal + "</h3>")
